8754925a3dfe96846ff298cb976a578c07a3111a
darkvical <vical.rl@gmail.com>
2015-07-11 19:29:26 -0500

    Creacion de eventos

--- app/src/main/java/vical/com/pe/myapp/Principal.java
+++ app/src/main/java/vical/com/pe/myapp/Principal.java
@@ -1,39 +1,43 @@
 package vical.com.pe.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Principal extends ActionBarActivity {
+
+    private EditText editMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        editMensaje = (EditText)findViewById(R.id.editMensaje);
     }
 
+    public void eventAceptar(View view){
+        String mensaje = editMensaje.getText().toString();
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_principal, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
